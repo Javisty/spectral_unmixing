5dc14667b3e827be0444d21f5ed125e89729dfca
Javisty <aymec.78620@gmail.com>
2022-02-23 23:55:30 +0100

    Implement objective function

--- src/objective.py
+++ src/objective.py
@@ -1,7 +1,7 @@
 """Define the objective function to minimize."""
 import torch
 import torch.nn as nn
-import numpy as np
+from .regularization import AbundanceRegularization, DispersionRegularization
 
 
 class SmoothedLinearUnmixing(nn.Module):
@@ -10,20 +10,38 @@ class SmoothedLinearUnmixing(nn.Module):
     regularization terms.
     """
 
-    def __init__(self, target, model, abundances):
+    def __init__(self, target, model, abundances, regu_ab, regu_model):
         """
         target: spectral image to fit
         model: model used to generate EMs spectra
         abundances: tensor of abundances
+        regu_ab: regularization model for abundances
+        regu_model: regularization model for model
         """
         super().__init__()
         self.target = target
         self.model = model
         self.A = abundances
+        self.N, self.M, self.P = abundances.size()  # dimensions of problem
+        self.regu_A = regu_ab
+        self.regu_E = regu_model
+
+    def reconstruction_error(self):
+        """
+        Compute the reconstruction error of the image based on the current
+        state.
+        """
+        abundances = self.A.reshape(self.N, self.M, self.P, 1)
+        reconstruction = torch.sum(self.model.forward() * abundances, dim=2)
+        return torch.sum((self.target - reconstruction)**2)
+
+    def regularization_penalty(self):
+        """Compute the regularization penalty based on the current state."""
+        return self.regu_A.forward() + self.regu_E.forward()
 
     def forward(self):
         """
-        Compute the value of the objective function based on the current
+        Compute the reconstruction error of the image based on the current
         state.
         """
-        pass
+        return self.reconstruction_error() + self.regularization_penalty()
